54fe8bfd759c57ae9a467cd894e9460e5fa7d921
Logan <logansailer0633@gamil.com>
2024-09-10 17:26:22 -0400

    changed post method to add to notes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let notes = [
   {
@@ -39,9 +39,29 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
 
