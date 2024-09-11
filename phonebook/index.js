3569301bf0927cbdfce6c135ff9f9b75b3b44509
Logan <logansailer0633@gamil.com>
2024-09-11 03:05:47 -0400

    added functionality in phonebook with morgan

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
 
-app.use(express.json());
-
-let phonebook = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -26,16 +25,80 @@ let phonebook = [
   },
 ];
 
-app.get("/api/phonebook", (request, response) => {
-  response.json(phonebook);
+app.use(express.json());
+app.use(morgan('tiny'))
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 
 app.get("/", (request, response) => {
   response.send("<h1>Welcome to Phonebook</h1>");
 });
 
+app.get("/info", (request, response) => {
+  let length = persons.length;
+  let date = new Date(Date.now());
+  console.log(response.date);
+  response.send(
+    `<p>Phonebook has info for ${length} people</p>
+    <p>${date}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+function getRandomID() {
+  return Math.floor(Math.random() * 10000);
+}
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  } else if (
+    persons.some((person) => person.name === body.name) ||
+    persons.some((person) => person.number === body.number)
+  ) {
+    return response.status(400).json({
+      error: "Name or number already in phonebook",
+    });
+  }
+
+  const person = {
+    id: getRandomID(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(persons);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
-
