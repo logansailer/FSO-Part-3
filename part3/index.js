4ee00a8b5dface5cfe4f6bf961753862868233b8
Logan <logansailer0633@gamil.com>
2024-09-13 01:18:54 -0400

    dist production

--- part3/index.js
+++ part3/index.js
@@ -30,6 +30,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(requestLogger);
 
 const unknownEndpoint = (request, response) => {
