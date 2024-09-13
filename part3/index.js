3a3ef4534229030dc184b665f194ab8d69c413b8
Logan <logansailer0633@gamil.com>
2024-09-13 00:50:37 -0400

    serving server

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 
 let notes = [
   {
@@ -27,6 +28,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(cors())
 app.use(express.json());
 app.use(requestLogger);
 
@@ -87,7 +89,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
