0abfced73d0fef49c8bd4eb6c79ecec9063a1c05
Logan <logansailer0633@gamil.com>
2024-09-17 02:06:34 -0400

    added env

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const url = `process.env.MONGODB_URI`;
+const url = process.env.MONGODB_URI;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
