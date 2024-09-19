1c56ee7ab439e47001b593066450cfd4ddcfc625
Logan <logansailer0633@gamil.com>
2024-09-19 16:47:14 -0400

    connected to backend

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,47 +1,39 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan")
+const morgan = require("morgan");
 const app = express();
-const cors = require('cors')
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(express.json());
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("dist"));
+
+const url = process.env.MONGODB_URI;
+console.log(url);
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: "String",
+  number: "String",
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
-
 app.get("/", (request, response) => {
   response.send("<h1>Welcome to Phonebook</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
